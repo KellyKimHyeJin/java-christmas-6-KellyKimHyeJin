4eec0148354abb2ba88c4a9d0bfba9758ce9e4fc
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-14 20:08:15 +0900

    feat: 혜택 출력 기능 구현

--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -7,5 +7,41 @@ public class OutputView {
         System.out.println("12월 "+ inputView.date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
         System.out.println();
         System.out.println("<주문 메뉴>");
+        System.out.println();
+    }
+
+    public void printTotalPrice(){
+        System.out.println("<할인 전 총주문 금액>");
+
+        System.out.println("원");
+        System.out.println();
+    }
+
+    public void printBonusMenu(){
+        System.out.println("<증정 메뉴>");
+
+        System.out.println();
+    }
+
+    public void printBenefit(){
+        System.out.println("<혜택 내역>");
+
+        System.out.println();
+    }
+
+    public void printTotalBenefit(){
+        System.out.println("<총혜택 금액>");
+
+        System.out.println();
+    }
+
+    public void printPrice(){
+        System.out.println("<할인 후 예상 결제 금액>");
+
+        System.out.println();
+    }
+
+    public void printBadge(){
+        System.out.println("<12월 이벤트 배지>");
     }
 }
