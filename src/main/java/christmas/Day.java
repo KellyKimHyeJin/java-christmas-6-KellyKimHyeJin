ed969ab8f29371d36926fd9b01537ca31531b615
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-14 19:59:00 +0900

    feat: Day ENUM 추가

--- src/main/java/christmas/Day.java
+++ src/main/java/christmas/Day.java
@@ -1,4 +1,20 @@
 package christmas;
 
 public enum Day {
+    MONDAY(0),
+    TUESDAY(1),
+    WEDNESDAY(2),
+    THURSDAY(3),
+    FRIDAY(4),
+    SATURDAY(5),
+    SUNDAY(6);
+
+    private int dayIdentify;
+    Day(int dayIdentify){
+        this.dayIdentify = dayIdentify;
+    }
+
+    public int getDayIdentify(){
+        return this.dayIdentify;
+    }
 }
