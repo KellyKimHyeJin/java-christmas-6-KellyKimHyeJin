c2b75f66c8a7b9fd971d2e042b2d19de6d52c34f
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-14 19:56:36 +0900

    feat: Menu ENUM 추가

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -2,6 +2,8 @@ package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.StringTokenizer;
+
 public class InputView {
 
     public int date;
@@ -20,6 +22,26 @@ public class InputView {
             }
         }
     }
+
+    public void inputMenu(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String totalMenu = Console.readLine();
+        StringTokenizer st = new StringTokenizer(totalMenu, ",-");
+        while(true){
+            try{
+                while(st.hasMoreTokens()){
+                    Menu menu = Menu.equalName(st.nextToken());
+                    menu.setMenuNumber(Integer.parseInt(st.nextToken()));
+                }
+                break;
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                totalMenu = Console.readLine();
+            }
+        }
+    }
+
+
 
 
 
--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -1,2 +1,47 @@
-package christmas;public enum Menu {
+package christmas;
+
+public enum Menu {
+    MUSHROOMSOUP("양송이수프",6000,"애피타이저",0),
+    TAPAS("타파스",5500,"애피타이저",0),
+    CAESARSALAD("시저샐러드",8000,"애피타이저",0),
+    TBONESTEAK("티본스테이크", 55000,"메인",0),
+    BBQRIBS("바비큐립",54000,"메인",0),
+    SEAFOODPASTA("해산물파스타",35000,"메인",0),
+    CHRISTMASPASTA("크리스마스파스타",25000,"메인",0),
+    CHOCOLATECAKE("초코케이크",15000,"디저트",0),
+    ICECREAM("아이스크림",5000,"디저트",0),
+    ZEROCOLA("제로콜라",3000,"음료",0),
+    REDWINE("레드와인",60000,"음료",0),
+    CHAMPAGNE("샴페인",25000,"음료",0);
+    private String name;
+    private int price;
+    private String kind;
+    private int number;
+
+    Menu(String name, int price, String kind, int number){
+        this.name = name;
+        this.price = price;
+        this.kind = kind;
+        this.number = number;
+    }
+
+    public void setMenuNumber(int number){
+        this.number = number;
+    }
+    public String getMenuName(){
+        return this.name;
+    }
+
+    public String getMenuKind(){
+        return this.kind;
+    }
+
+    public static Menu equalName(String name){
+        for(Menu menu: Menu.values()){
+            if(menu.getMenuName().equals(name)){
+                return menu;
+            }
+        }
+        return null;
+    }
 }
