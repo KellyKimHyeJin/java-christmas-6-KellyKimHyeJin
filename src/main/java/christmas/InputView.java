26d66c2d6a5193dc802ff511107745c7d2c9a646
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-14 20:01:54 +0900

    feat: 주문 메뉴 출력 기능 구현

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -23,27 +23,22 @@ public class InputView {
         }
     }
 
-    public void inputMenu(){
+    public void inputMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String totalMenu = Console.readLine();
         StringTokenizer st = new StringTokenizer(totalMenu, ",-");
-        while(true){
-            try{
-                while(st.hasMoreTokens()){
+        while (true) {
+            try {
+                while (st.hasMoreTokens()) {
                     Menu menu = Menu.equalName(st.nextToken());
                     menu.setMenuNumber(Integer.parseInt(st.nextToken()));
                 }
                 break;
-            } catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
                 totalMenu = Console.readLine();
             }
         }
     }
-
-
-
-
-
 
 }
--- src/main/java/christmas/OutputView.java
+++ src/main/java/christmas/OutputView.java
@@ -1,4 +1,11 @@
 package christmas;
 
 public class OutputView {
+    InputView inputView = new InputView();
+
+    public void printMenu(){
+        System.out.println("12월 "+ inputView.date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
+        System.out.println("<주문 메뉴>");
+    }
 }
