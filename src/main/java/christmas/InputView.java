6fdb8ce2f7d40a3ed37f633ff31679dcb6f50e46
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-15 21:31:44 +0900

    feat: InputView 예외처리

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -12,15 +12,22 @@ public class InputView {
     public void inputDate(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        date = Integer.parseInt(Console.readLine());
+        String dateString = Console.readLine();
         while(true){
+            try{
+                date = Integer.parseInt(dateString);
+                break;
+            } catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                dateString = Console.readLine();
+            }
             try{
                 if(date <= 31 && date >= 1) break;
                 IllegalArgumentException e = new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
                 throw e;
             } catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
-                date = Integer.parseInt(Console.readLine());
+                dateString = Console.readLine();
             }
         }
     }
